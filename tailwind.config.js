f94cc0d15aebb83585ebdf5529125a5a15e00002
ReDay Zarra <redayzarra@gmail.com>
2023-01-05 17:50:08 -0500

    Adding build and watch scripts to json files and starting index.html file

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], /*Tailwind is going to look for any HTML files*/
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
